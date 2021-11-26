68fcdadcd921e87078a20eb3e8c943617c39d97d
Clement Benezech <clementbenezech@gmail.com>
2021-11-26 18:35:08 +0100

    Added a generic PageContent component, already including header, footer and nav. Then, we use this component in the routes and we give it specific content by nesting children

--- src/components/PageContent.jsx
+++ src/components/PageContent.jsx
@@ -1,4 +1,15 @@
-const PageContent = () => {
-    return <div>PageContent</div>
+import Header from "./Header"
+import Footer from "./Footer"
+
+const PageContent = (props) => {
+    return (
+    <fragment>
+                <Header/>
+                  <div className = "accommodation">
+                        {props.children}
+                  </div>
+                <Footer/>
+            </fragment>
+    )
 }
 export default PageContent
--- src/router/AppRoutes.js
+++ src/router/AppRoutes.js
@@ -6,11 +6,7 @@ import PageNotFound from '../pages/notFound/PageNotFound';
 import ResultGrid from '../pages/home/ResultGrid';
 import CoreValues from '../pages/aboutUs/CoreValues';
 
-
-import Header from '../components/Header';
-import Footer from '../components/Footer';
 import PictureBox from '../components/PictureBox';
-
 
 import homeImage from '../images/home.png';
 import aboutImage from '../images/about.png'
@@ -23,55 +19,51 @@ import { NOT_FOUND } from "./constants/paths";
 
 //For now we do this but later we'll set up a new Page Content component handling all this.
 //import PageContent from './PageContent
-/*import PageContent from "../components/PageContent";*/
+import PageContent from "../components/PageContent";
 
 const AppRoutes = (setCurrentAccommodation, accommodation) => {
     return (
-        <Routes>
-            <Route path={ACCOMMODATION_PAGE} element = {
-                <fragment>
-                <Header/>
-                  <div className = "accommodation">
-                    <Carousel accommodation = {accommodation}/>
-                    <AccommodationDetails accommodation = {accommodation}/>
-                  </div>
-                <Footer/>
-            </fragment>
-                }>
-            </Route>
+            <Routes>
+                  <Route path={ACCOMMODATION_PAGE} element = {
+                      <PageContent>
+                        <div className = "accommodation">
+                          <Carousel accommodation = {accommodation}/>
+                          <AccommodationDetails accommodation = {accommodation}/>
+                        </div>
+                      </PageContent>
+                      }>
+                  </Route>
 
-            <Route path= {ABOUT_US} element = {
-            <fragment>
-            <Header/>
-              <div className = "about-us">
-                  <PictureBox key = "about" image = {aboutImage} width = "90%" height = "20vh" margin = " 0 5%"/>
-                  <CoreValues/>
-              </div>
-            <Footer/>
-          </fragment>
-            }>
-            </Route>
+                  <Route path= {ABOUT_US} element = {
+                      <PageContent>
+                      <div className = "about-us">
+                            <PictureBox key = "about" image = {aboutImage} width = "90%" height = "20vh" margin = " 0 5%"/>
+                            <CoreValues/>
+                      </div>
+                      </PageContent>
+                  }>
+                  </Route>
                 
-            <Route exact path={HOMEPAGE} element = {
-            <fragment>
-            <Header/>
-              <div className = "homepage">
-                  <PictureBox key = "home" image = {homeImage} width = "90%" height = "20vh" margin = " 0 5%" text = "Chez vous, partout et ailleurs."/>
-                  <ResultGrid setCurrentAccommodation = {setCurrentAccommodation}/>
-              </div>
-            <Footer/>
-          </fragment>
-            }>
-            </Route>
+                  <Route exact path={HOMEPAGE} element = {
 
-            <Route path= {NOT_FOUND} element = {
-                    <fragment>
-                    <Header/>
-                    <PageNotFound/>
-                    <Footer/>
-             </fragment>
-            }>
-            </Route>
+                      <PageContent>
+                        <div className = "homepage">
+                          <PictureBox key = "home" image = {homeImage} width = "90%" height = "20vh" margin = " 0 5%" text = "Chez vous, partout et ailleurs."/>
+                          <ResultGrid setCurrentAccommodation = {setCurrentAccommodation}/>
+                        </div>
+                      </PageContent>
+                  }>
+                  </Route>
+
+                  <Route path= {NOT_FOUND} element = {
+                    <PageContent>
+                        <div className = "page-not-found">
+                            <PageNotFound/>
+                        </div>
+                    </PageContent>
+
+                  }>
+                  </Route>
             </Routes>
     )
 }
